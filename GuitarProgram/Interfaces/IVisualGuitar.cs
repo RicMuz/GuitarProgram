d29aa04dc6dd86587d36d63fb97b048060bc5668
muzikr <richard@imuzik.cz>
2022-08-09 17:25:13 +0200

    Created interface for graphical representation

---
+++ GuitarProgram/Interfaces/IVisualGuitar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarProgram
+{
+    internal interface IVisualGuitar
+    {
+        /// <summary>
+        /// Graphicaly shows how given chord is hold on fretboard
+        /// </summary>
+        /// <param name="ChordShape">Array of 6 ints representing given tones on guitar fretboard</param>
+        public void ShowChord(int[] ChordShape);
+
+        /// <summary>
+        /// Resets the graphical representation like if no chord is hold
+        /// </summary>
+        public void Reset();
+    }
+}
