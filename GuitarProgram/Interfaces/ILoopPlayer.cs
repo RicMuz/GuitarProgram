f09ced2f95bceb611909892b43e562882cd550c9
muzikr <richard@imuzik.cz>
2022-08-10 22:12:15 +0200

    Created player interface

---
+++ GuitarProgram/Interfaces/ILoopPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarProgram
+{
+    internal interface ILoopPlayer
+    {
+        /// <summary>
+        /// Sets given loop to current loop
+        /// </summary>
+        public List<Chord> Loop { set; }
+
+        /// <summary>
+        /// Converts given text to loop
+        /// </summary>
+        /// <param name="input">Input text in defined format</param>
+        public void LoadLoop(string[] input);
+
+        /// <summary>
+        /// Increases pointer and returns next chord
+        /// </summary>
+        /// <returns>Next chord in loop</returns>
+        public Chord NextChord();
+
+        /// <summary>
+        /// Resets pointer of the loop to the beggining
+        /// </summary>
+        public void Reset();
+    }
+}
