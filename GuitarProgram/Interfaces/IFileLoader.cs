91034c10e77d2c39e9b645bf3619efaadd8ff75c
muzikr <richard@imuzik.cz>
2022-08-10 21:39:29 +0200

    Created interface for file loader

---
+++ GuitarProgram/Interfaces/IFileLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarProgram
+{
+    internal interface IFileLoader
+    {
+        /// <summary>
+        /// Reads and processes the file
+        /// </summary>
+        /// <param name="FileName">Name of wanted file</param>
+        /// <returns>List of chords to be play in loop</returns>
+        public List<Chord> ReadFile(string FileName);
+    }
+}
